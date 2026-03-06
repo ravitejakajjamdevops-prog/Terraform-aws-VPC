4c70e7fbb36dd817e9c776d0b6d8538dfc0b4c9f
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-07 02:42:03 +0530

    terr

---
+++ locals.tf
@@ -0,0 +1,17 @@
+locals {
+    common_tags = {
+
+        Project = var.project
+        Environment = var.environment
+        Terraform = true
+    }
+
+    vpc_final_tags = merge ( 
+        local.common_tags,
+        {
+           Name = "${var.project}-${var.environment}"
+        }
+    )    
+    
+
+}
