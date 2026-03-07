6a8ad2e58f580d88d973d8076a2f1f2676e58a3b
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-07 16:39:21 +0530

    fff

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "roboshop_public" {
   count = length(var.Public_subnet)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.Public_subnet[count.index]
-
+  availability_zone = local.az_names[count.index]
   tags = local.vpc_final_tags
 }
 
