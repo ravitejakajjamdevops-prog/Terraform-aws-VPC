c43cd9b82ae02d305983198b7e93c98fd456b309
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-07 17:00:17 +0530

    fff

--- main.tf
+++ main.tf
@@ -14,7 +14,12 @@ resource "aws_subnet" "roboshop_public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.Public_subnet[count.index]
   availability_zone = local.az_names[count.index]
-  tags = local.vpc_final_tags
+  tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        }
+  ) 
 }
 
 
