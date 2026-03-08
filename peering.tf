d2f606615dec8df568d33a305675939ed2e22fea
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-08 17:19:07 +0530

    Removed terraform provider files

--- peering.tf
+++ peering.tf
@@ -20,21 +20,21 @@ resource "aws_vpc_peering_connection" "default" {
 }
 resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.public.id
+  route_table_id            = aws_route_table.Public_routet.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
 }
 
 resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.private.id
+  route_table_id            = aws_route_table.Private_routetable.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
 }
 
 resource "aws_route" "database_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.database.id
+  route_table_id            = aws_route_table.database_routetable.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
 }
